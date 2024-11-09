48db1c73f341eec973b5248c1bb03515a438380b
User <User@DESKTOP-0TCDSR6>
2024-11-09 20:18:06 +0300

    Window Size & Position DONE

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,5 +1,7 @@
 ﻿//MainWindow
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
 
 CONST CHAR g_sz_MY_WINDOW_CLASS[] = "My Window";	//Имя класса окна
 
@@ -33,14 +35,24 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	}
 
 	//2) Создаение окна:
+
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+	//https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getsystemmetrics?redirectedfrom=MSDN
+
+	INT window_width = screen_width * 3 / 4;
+	INT window_height = screen_height * .75;
+	INT window_start_x = screen_width / 8;
+	INT window_start_y = screen_height / 8;
+
 	HWND hwnd = CreateWindowEx
 	(
 		NULL,		//ExStyles
 		g_sz_MY_WINDOW_CLASS,	//Class name
 		g_sz_MY_WINDOW_CLASS,	//Window title
 		WS_OVERLAPPEDWINDOW,	//Window style
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Window position
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Window size
+		window_start_x, window_start_y,	//Window position
+		window_width, window_height,	//Window size
 		NULL,	//Parent Window
 		NULL,	//Main menu ResourceID for MainWindow or ResourceID for ChildWindow
 		hInstance,
@@ -67,6 +79,43 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		break;
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		INT window_width = rect.right - rect.left;
+		INT window_height = rect.bottom - rect.top;
+
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+		sprintf
+		(
+			sz_title,
+			"%s - Position: %ix%i;\tSize: %ix%i",
+			g_sz_MY_WINDOW_CLASS,
+			rect.left, rect.top,
+			window_width,window_height
+		);
+		//https://legacy.cplusplus.com/reference/cstdio/sprintf/
+		/*
+		--------------------------
+		Функция sprintf(dst, src, parameters); форматирует src-строку, и результат форматирования сохраняет в dst-строку.
+		dst - Destination (строка получатель);
+		src - Source (строка источник). Как правило является форматированной строкой.
+			  Форматированная строка как правило содержит спецификаторы,
+			  которые подменяются соответствуюшими параметрами.
+				%i - на место этого спецификатора вставляется значение типа 'int'
+				%s - на место этого спецификатора вставляется значение типа 'char*'
+		s... - string;
+		...f - format;
+		printf();	// Print formatted string (вывести на экран отформатированную строку).
+		//https://legacy.cplusplus.com/reference/cstdio/printf/
+		--------------------------
+		*/
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}
+	break;
 	case WM_COMMAND:
 		break;
 	case WM_DESTROY:
