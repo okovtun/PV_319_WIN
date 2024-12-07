fab7a8e54f2482f7f423e4e59601be85b3ad4be2
User <User@DESKTOP-0TCDSR6>
2024-12-07 21:00:53 +0300

    Portable release DONE

--- WinAPI/Calc/main.cpp
+++ WinAPI/Calc/main.cpp
@@ -1,7 +1,10 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include<iostream>
+#include<Shlwapi.h>
 #include"resource.h"
+
+#pragma comment (lib, "Shlwapi.lib")
 
 CONST CHAR g_sz_CLASS_NAME[] = "Calc PV_319";
 
@@ -35,6 +38,7 @@ CONST COLORREF g_WINDOW_BACKGROUND[] = { RGB(0, 0, 150), RGB(75,  75, 75) };
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 VOID SetSkin(HWND hwnd, CONST CHAR* skin);
 VOID SetSkinFromDLL(HWND hwnd, CONST CHAR* skin);
+VOID GetExeDirectory(CHAR* buffer, DWORD size);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -132,7 +136,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		AddFontResourceEx("Fonts\\Calculator.ttf", FR_PRIVATE, 0);
 		//////////////////////////////////////////////////
 		//https://stackoverflow.com/questions/58712918/win32-use-a-resource-font-inside-the-application
-		HINSTANCE hInstFont = LoadLibrary("..\\Debug\\FontOnlyDLL.dll");
+		CHAR filepath[MAX_PATH]{};
+		GetExeDirectory(filepath, MAX_PATH);
+		CHAR dllpath[MAX_PATH]{};
+		PathCombine(dllpath, filepath, ("Fonts\\Terminator2.dll"));
+		HINSTANCE hInstFont = LoadLibrary(dllpath);
+		//HINSTANCE hInstFont = LoadLibrary("..\\Debug\\FontOnlyDLL.dll");
 		HRSRC hFontRes = FindResource(hInstFont, MAKEINTRESOURCE(99), "BINARY");
 		HGLOBAL hFntMem = LoadResource(hInstFont, hFontRes);
 		VOID* fntData = LockResource(hFntMem);
@@ -272,7 +281,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			GetModuleHandle(NULL),
 			NULL
 		);
-		SetSkin(hwnd, "square_blue");
+		SetSkinFromDLL(hwnd, "square_blue");
 	}
 	break;
 	case WM_CTLCOLOREDIT:
@@ -589,8 +598,10 @@ VOID SetSkin(HWND hwnd, CONST CHAR* skin)
 }
 VOID SetSkinFromDLL(HWND hwnd, CONST CHAR* skin)
 {
+	CHAR filepath[MAX_PATH]{};
 	CHAR filename[MAX_PATH]{};
-	sprintf(filename, "ButtonsBMP\\%s", skin);
+	GetExeDirectory(filepath, MAX_PATH);
+	sprintf(filename, "%s\\ButtonsBMP\\%s", filepath, skin);
 	HMODULE hInst = LoadLibrary(filename);
 	for (int i = IDC_BUTTON_0; i <= IDC_BUTTON_EQUAL; i++)
 	{
@@ -606,4 +617,9 @@ VOID SetSkinFromDLL(HWND hwnd, CONST CHAR* skin)
 		SendMessage(GetDlgItem(hwnd, i), BM_SETIMAGE, IMAGE_BITMAP, (LPARAM)buttonBMP);
 	}
 	FreeLibrary(hInst);
+}
+VOID GetExeDirectory(CHAR* buffer, DWORD size)
+{
+	GetModuleFileName(NULL, buffer, size);
+	PathRemoveFileSpec(buffer);
 }
